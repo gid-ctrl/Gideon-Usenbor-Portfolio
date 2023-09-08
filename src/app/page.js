7333bc415c03573d0cca1d7ff354bf9b88607a1e
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-09-08 17:06:17 +0100

    working on navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Navbar from "./components/navbar";
 import HeroSection from "./components/herosection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black container mx-auto py-4 px-12">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
